f7f7b3da86806f02e67da2a5deddf409ca24cac7
DavidNabet <43647871+DavidNabet@users.noreply.github.com>
2021-09-22 23:00:07 +0200

    styles comics changed

--- src/components/ComicCard.js
+++ src/components/ComicCard.js
@@ -2,48 +2,49 @@ import { useState, useEffect } from "react";
 import StarIcon from "./StarIcon";
 
 const ComicCard = ({ id, result, addToFavorite }) => {
-  const [favoris, setFavoris] = useState("star");
+	const [favoris, setFavoris] = useState("star");
 
-  useEffect(() => {
-    if (localStorage.getItem("favorisComics") !== null) {
-      // Si l'id du résultat existe dans le localStorage
-      if (localStorage.getItem("favorisComics").includes(result._id)) {
-        if (result._id === id) {
-          // Alors on peut changer la valeur
-          setFavoris(!favoris);
-        }
-      }
-    } else {
-      localStorage.removeItem("favorisComics");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+	useEffect(() => {
+		if (localStorage.getItem("favorisComics") !== null) {
+			// Si l'id du résultat existe dans le localStorage
+			if (localStorage.getItem("favorisComics").includes(result._id)) {
+				if (result._id === id) {
+					// Alors on peut changer la valeur
+					setFavoris(!favoris);
+				}
+			}
+		} else {
+			localStorage.removeItem("favorisComics");
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <div key={result._id} className="comics__inner">
-      <span
-        id="pos_star"
-        onClick={(e) => {
-          addToFavorite(e, "comics", result);
-          setFavoris(!favoris);
-        }}
-      >
-        <StarIcon className={favoris ? "star" : "starRed"} />
-      </span>
-      <img
-        src={`${result.thumbnail.path}.${result.thumbnail.extension}`}
-        alt=""
-      />
-      <div className="comics_details">
-        <h3>{result.title}</h3>
-        {result.description ? (
-          <p>{result.description.slice(0, 50) + "..."}</p>
-        ) : (
-          <p style={{ textAlign: "center" }}>Aucune description</p>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<figure className="characters__inner">
+			<span
+				id="pos_star"
+				onClick={(e) => {
+					addToFavorite(e, "comics", result);
+					setFavoris(!favoris);
+				}}
+			>
+				<StarIcon className={favoris ? "star" : "starRed"} />
+			</span>
+			<>
+				<img
+					src={`${result.thumbnail.path}.${result.thumbnail.extension}`}
+					alt=""
+				/>
+				<figcaption>
+					{result.description ? (
+						<p>{result.description.slice(0, 300) + "..."}</p>
+					) : (
+						<p style={{ textAlign: "center" }}>Aucune description</p>
+					)}
+				</figcaption>
+			</>
+		</figure>
+	);
 };
 
 export default ComicCard;
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -3,76 +3,76 @@ import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Signup = ({ setUserToken }) => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const history = useHistory();
-  let obj = {};
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const history = useHistory();
+	let obj = {};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      //On reset setErrorMessage pour annuler l'erreur
-      setErrorMessage("");
-      const response = await axios.post(
-        "https://marvel-back-project.herokuapp.com/user/signup",
-        {
-          username: username,
-          email: email,
-          password: password,
-        }
-      );
-      if (response.data) {
-        obj.token = response.data.token;
-        obj.username = response.data.username;
-        setUserToken(obj);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			//On reset setErrorMessage pour annuler l'erreur
+			setErrorMessage("");
+			const response = await axios.post(
+				"https://marvel-back-project.herokuapp.com/user/signup",
+				{
+					username: username,
+					email: email,
+					password: password,
+				}
+			);
+			if (response.data) {
+				obj["token"] = response.data.token;
+				obj["username"] = response.data.username;
+				setUserToken(obj);
 
-        history.push("/");
-      }
-    } catch (error) {
-      // On affiche tous les messages d'erreurs présents côté serveur
-      if (error.response.status === 409) {
-        setErrorMessage("Cet email possède déjà un compte");
-      } else if (
-        error.response.data.message === "This username is already used"
-      ) {
-        setErrorMessage("Ce Pseudo existe déjà");
-      } else {
-        setErrorMessage("Une erreur est survenue");
-      }
-      console.log(error.message);
-    }
-  };
+				history.push("/");
+			}
+		} catch (error) {
+			// On affiche tous les messages d'erreurs présents côté serveur
+			if (error.response.status === 409) {
+				setErrorMessage("Cet email possède déjà un compte");
+			} else if (
+				error.response.data.message === "This username is already used"
+			) {
+				setErrorMessage("Ce Pseudo existe déjà");
+			} else {
+				setErrorMessage("Une erreur est survenue");
+			}
+			console.log(error.message);
+		}
+	};
 
-  return (
-    <form onSubmit={handleSubmit} className="form form_signup">
-      <h2>Inscrivez-vous !</h2>
-      <input
-        type="text"
-        value={username}
-        placeholder="Pseudo"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="email"
-        value={email}
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        placeholder="Mot de passe"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {errorMessage && <p className="invalid-feedback">{errorMessage}</p>}
-      <input type="submit" value="Envoyer" />
-      <Link to="/login">
-        <p>Déjà un compte ? Connectez-vous !</p>
-      </Link>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit} className="form form_signup">
+			<h2>Inscrivez-vous !</h2>
+			<input
+				type="text"
+				value={username}
+				placeholder="Pseudo"
+				onChange={(e) => setUsername(e.target.value)}
+			/>
+			<input
+				type="email"
+				value={email}
+				placeholder="Email"
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<input
+				type="password"
+				value={password}
+				placeholder="Mot de passe"
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			{errorMessage && <p className="invalid-feedback">{errorMessage}</p>}
+			<input type="submit" value="Envoyer" />
+			<Link to="/login">
+				<p>Déjà un compte ? Connectez-vous !</p>
+			</Link>
+		</form>
+	);
 };
 
 export default Signup;
